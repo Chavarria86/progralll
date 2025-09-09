a1be9cf599d4c4848df1650cef4d9556d4b54568
chavarria86 <USSS027724@ugb.edu.sv>
2025-09-09 08:42:21 -0600

    Primer Hola Mundo WEB

---
+++ server.py
@@ -0,0 +1,11 @@
+from http.server import HTTPServer, SimpleHTTPRequestHandler
+port = 3000
+class miservidor(SimpleHTTPRequestHandler):
+    def do_GET(self):
+        if self.path=="/":
+            self.path="/index.html"
+            return SimpleHTTPRequestHandler.do_GET(self)
+
+print("Servidor ejecutandose en el puerto 3000",port)       
+server = HTTPServer({"localhost",port},miservidor)
+server.serve_forever() 
